169127e1613074f665ed9ba69e9dcd02d9aa04a0
andwah <anders.wahlgren@societyobjects.com>
2014-10-16 14:27:52 +0200

    Added SystemUserGroupBasedOn

---
+++ src/SocietyObjects/Concepts.Ring5/SystemX/SystemUserGroupBasedOn.cs
@@ -0,0 +1,31 @@
+﻿using Concepts.Ring1;
+using Concepts.Ring3;
+using Starcounter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concepts.Ring5 {
+
+    [Database]
+    public class SystemUserGroupBasedOn : Relation {
+
+        /// <summary>
+        /// A SystemUserGroup based on another SystemUserGroup
+        /// </summary>
+        [SynonymousTo("WhatIs")]
+        public readonly SystemUserGroup SystemUserGroup;
+        public void SetSystemUserGroup(SystemUserGroup SystemUserGroup) {
+            SetWhatIs(SystemUserGroup);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [SynonymousTo("ToWhat")]
+        public readonly SystemUserGroup SystemUserGroupBaseOn;
+    }
+}
