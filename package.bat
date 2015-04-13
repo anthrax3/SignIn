f975df08445d25765f96e538e382704eebc782cc
miyconst <const.miyang@gmail.com>
2015-04-13 20:30:16 +0300

    Fix project structure

--- package.bat
+++ package.bat
@@ -17,21 +17,21 @@ md "%~dp0temp\wwwroot"
 xcopy "%~dp0src\SignIn\wwwroot" "%~dp0temp\wwwroot" /s /e
 
 REM Copy icon and config
-xcopy "%~dp0src\package\*.png" "%~dp0temp"
-xcopy "%~dp0src\package\*.config" "%~dp0temp"
+xcopy "%~dp0src\SignIn\package\*.png" "%~dp0temp"
+xcopy "%~dp0src\SignIn\package\*.config" "%~dp0temp"
 
 REM Get folder name for the zip name
 for %%a in ("%~dp0.") do set currentfolder=%%~na
 
 
-IF NOT EXIST "%~dp0dist" (
- md "%~dp0dist"
+IF NOT EXIST "%~dp0packages" (
+ md "%~dp0packages"
 )
 
 REM Zipp-it
 CD "%~dp0temp"
 
-zip -r "..\dist\%currentfolder%" *.*  || (GOTO ERROR)
+zip -r "..\packages\%currentfolder%" *.*  || (GOTO ERROR)
 
 REM TODO Jump back to previous dir
 GOTO :CLEANUP
