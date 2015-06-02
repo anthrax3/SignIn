dd605a446fdb70cefc30c22dfda74c7f9a7d2379
miyconst <const.miyang@gmail.com>
2015-06-02 10:37:24 +0300

    Refactor handlers code a little bit

--- src/SignIn/Api/MainHandlers.cs
+++ src/SignIn/Api/MainHandlers.cs
@@ -13,57 +13,6 @@ namespace SignIn {
         protected string AuthCookieName = "soauthtoken";
 
         public void Register() {
-            Starcounter.Handle.GET("/signin/user", () => {
-                SessionContainer container = this.GetSessionContainer();
-                //Session session = Session.Current;
-
-                if (container.SignIn != null) {
-                    return container.SignIn;
-                }
-
-                List<Cookie> cookies = Handle.IncomingRequest.Cookies.Select(x => new Cookie(x)).ToList();
-                Cookie cookie = cookies.FirstOrDefault(x => x.Name == AuthCookieName);
-                SignInPage page = new SignInPage();
-
-                if (cookie != null) {
-                    page.FromCookie(cookie.Value);
-                } else {
-                    page.SetAnonymousState();
-                }
-
-                //page.Session = session;
-                container.SignIn = page;
-
-                //Testing JWT
-                /*if (Handle.IncomingRequest.HeadersDictionary.ContainsKey("x-jwt")) {
-                    System.Web.Script.Serialization.JavaScriptSerializer serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
-                    string jwt = Handle.IncomingRequest.HeadersDictionary["x-jwt"];
-                    Dictionary<string, string> payload = JWT.JsonWebToken.DecodeToObject<Dictionary<string, string>>(jwt, string.Empty, false);
-                    string username = payload["Username"];
-                    SystemUser user = Db.SQL<SystemUser>("SELECT su FROM Simplified.Ring3.SystemUser su WHERE su.Username = ?", username).First;
-
-                    try {
-                        JWT.JsonWebToken.DecodeToObject<Dictionary<string, string>>(jwt, user.Password, true);
-                        page.SetAuthorizedState(SignInOut.SignInSystemUser(user));
-                    } catch (JWT.SignatureVerificationException) { 
-                    }
-                }*/
-
-                return page;
-            });
-
-            Handle.GET("/signin/signin/{?}/{?}", (string Username, string Password) => {
-                SessionContainer container = this.GetSessionContainer();
-                //SignInPage page = Session.Current.Data as SignInPage;
-
-                //page.SignIn(Username, Password);
-                container.SignIn.SignIn(Username, Password);
-                SetAuthCookie(container.SignIn);
-
-                //return page.SignInForm;
-                return container.SignInForm;
-            });
-
             //Testing JWT
             /*Handle.GET("/signin/jwt/{?}/{?}", (string Username, string Password) => {
                 string message;
@@ -77,52 +26,21 @@ namespace SignIn {
                 return 200;
             });*/
 
-            Handle.GET("/signin/signout", () => {
-                SessionContainer container = this.GetSessionContainer();
-                //SignInPage page = Session.Current.Data as SignInPage;
-
-                //page.SignOut();
-                container.SignIn.SignOut();
-                SetAuthCookie(container.SignIn);
-
-                //return page.SignInForm;
-                return container.SignInForm;
-            });
-
-            Handle.GET("/signin/signinuser", () => {
-                SignInPage master = Self.GET<Page>("/signin/user") as SignInPage;
-                SignInFormPage page = new SignInFormPage();
-                SessionContainer container = this.GetSessionContainer();
-
-                container.SignInForm = page;
-                master.UpdateSignInForm();
-
-                return page;
-            });
-
-            Handle.GET("/signin/signinuser?{?}", (string query) => {
-                SignInPage master = Self.GET<Page>("/signin/user") as SignInPage;
-                SignInFormPage page = new SignInFormPage();
-                string decodedQuery = HttpUtility.UrlDecode(query);
-                NameValueCollection queryCollection = HttpUtility.ParseQueryString(decodedQuery);
-                SessionContainer container = this.GetSessionContainer();
-
-                page.OriginUrl = queryCollection.Get("originurl");
-                container.SignInForm = page;
-                master.UpdateSignInForm();
-
-                return page;
-            });
+            Handle.GET("/signin/user", HandleUser);
+            Handle.GET<string, string>("/signin/signin/{?}/{?}", HandleSignIn);
+            Handle.GET("/signin/signout", HandleSignOut);
+            Handle.GET("/signin/signinuser", HandleSignInUser);
+            Handle.GET<string>("/signin/signinuser?{?}", HandleSignIn);
 
             //Test handler
-            Handle.GET("/signin/deleteadminuser", () => {
+            /*Handle.GET("/signin/deleteadminuser", () => {
                 Db.Transact(() => {
                     Db.SlowSQL("DELETE FROM Simplified.Ring3.SystemUserGroupMember WHERE SystemUser.Username = ?", SignInOut.AdminUsername);
                     Db.SlowSQL("DELETE FROM Simplified.Ring3.SystemUser WHERE Username = ?", SignInOut.AdminUsername);
                 });
 
                 return 200;
-            });
+            });*/
 
             Polyjuice.Map("/signin/user", "/polyjuice/user");
         }
@@ -153,6 +71,86 @@ namespace SignIn {
             }
 
             return container;
+        }
+
+        protected Response HandleSignIn(string Username, string Password) {
+            SessionContainer container = this.GetSessionContainer();
+
+            container.SignIn.SignIn(Username, Password);
+            SetAuthCookie(container.SignIn);
+
+            return container.SignInForm;
+        }
+
+        protected Response HandleSignIn(string Query) {
+            SignInPage master = Self.GET<Page>("/signin/user") as SignInPage;
+            SignInFormPage page = new SignInFormPage();
+            string decodedQuery = HttpUtility.UrlDecode(Query);
+            NameValueCollection queryCollection = HttpUtility.ParseQueryString(decodedQuery);
+            SessionContainer container = this.GetSessionContainer();
+
+            page.OriginUrl = queryCollection.Get("originurl");
+            container.SignInForm = page;
+            master.UpdateSignInForm();
+
+            return page;
+        }
+
+        protected Response HandleSignInUser() {
+            SignInPage master = Self.GET<Page>("/signin/user") as SignInPage;
+            SignInFormPage page = new SignInFormPage();
+            SessionContainer container = this.GetSessionContainer();
+
+            container.SignInForm = page;
+            master.UpdateSignInForm();
+
+            return page;
+        }
+
+        protected Response HandleSignOut() {
+            SessionContainer container = this.GetSessionContainer();
+
+            container.SignIn.SignOut();
+            SetAuthCookie(container.SignIn);
+
+            return container.SignInForm;
+        }
+
+        protected Response HandleUser() {
+            SessionContainer container = this.GetSessionContainer();
+
+            if (container.SignIn != null) {
+                return container.SignIn;
+            }
+
+            List<Cookie> cookies = Handle.IncomingRequest.Cookies.Select(x => new Cookie(x)).ToList();
+            Cookie cookie = cookies.FirstOrDefault(x => x.Name == AuthCookieName);
+            SignInPage page = new SignInPage();
+
+            if (cookie != null) {
+                page.FromCookie(cookie.Value);
+            } else {
+                page.SetAnonymousState();
+            }
+
+            container.SignIn = page;
+
+            //Testing JWT
+            /*if (Handle.IncomingRequest.HeadersDictionary.ContainsKey("x-jwt")) {
+                System.Web.Script.Serialization.JavaScriptSerializer serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+                string jwt = Handle.IncomingRequest.HeadersDictionary["x-jwt"];
+                Dictionary<string, string> payload = JWT.JsonWebToken.DecodeToObject<Dictionary<string, string>>(jwt, string.Empty, false);
+                string username = payload["Username"];
+                SystemUser user = Db.SQL<SystemUser>("SELECT su FROM Simplified.Ring3.SystemUser su WHERE su.Username = ?", username).First;
+
+                try {
+                    JWT.JsonWebToken.DecodeToObject<Dictionary<string, string>>(jwt, user.Password, true);
+                    page.SetAuthorizedState(SignInOut.SignInSystemUser(user));
+                } catch (JWT.SignatureVerificationException) { 
+                }
+            }*/
+
+            return page;
         }
     }
 }
