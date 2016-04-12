46dba45096589556adde00bd964cb3e45b992fe9
Konstantin <const.miyang@gmail.com>
2016-04-12 12:54:29 +0200

    Rework sign in form

--- src/SignIn/ViewModels/SignInFormPage.json.cs
+++ src/SignIn/ViewModels/SignInFormPage.json.cs
@@ -3,5 +3,22 @@ using Starcounter;
 
 namespace SignIn {
     partial class SignInFormPage : Page {
+        void Handle(Input.SignInClick Action) {
+            string message = null;
+
+            this.Message = null;
+            Action.Cancel();
+
+            if (string.IsNullOrEmpty(this.Username)) {
+                message = "Username is required!";
+            }
+
+            if (!string.IsNullOrEmpty(message)) {
+                this.Message = message;
+                return;
+            }
+
+            this.Submit++;
+        }
     }
 }
