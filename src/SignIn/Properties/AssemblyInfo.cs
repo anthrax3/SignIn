a8f36a64bb0decf238e21219693d1cf8814ad2c3
andwah <anders.wahlgren@societyobjects.com>
2016-11-17 17:02:15 +0100

    Changed the namespace to StarcounterPrefabs

--- src/SignIn/Properties/AssemblyInfo.cs
+++ src/SignIn/Properties/AssemblyInfo.cs
@@ -6,11 +6,10 @@ using Starcounter.Internal;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: AssemblyTitle("SignIn")]
+[assembly: AssemblyTitle("Sign In")]
 [assembly: AssemblyDescription("Authenticate **users** with a username and password (default admin/admin).")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("StarcounterSamples")]
+[assembly: AssemblyCompany("StarcounterPrefabs")]
 [assembly: AssemblyProduct("SignIn")]
 [assembly: AssemblyCopyright("Copyright 2016 Starcounter")]
 [assembly: AssemblyTrademark("")]
@@ -19,11 +18,9 @@ using Starcounter.Internal;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
 [assembly: Guid("526edd06-b4f0-469f-a4d4-e95b3c94f5ac")]
 
 // Version information for an assembly consists of the following four values:
@@ -40,10 +37,11 @@ using Starcounter.Internal;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("5.0.4")]
 
+
 // Starcounter Package information
 //
 // Unique warehouse id e.g. <myorganization.myapp>
-[assembly: AssemblyMetadata("ID", "StarcounterSamples.SignIn")]
+[assembly: AssemblyMetadata("ID", "StarcounterPrefabs.SignIn")]
 
 // Assures the current assembly has a reference to the Starcounter
 // assembly. A reference to Starcounter is currently required for
