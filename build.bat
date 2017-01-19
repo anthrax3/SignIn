44f8f1d4c11da4dae21425e76e998a1c62cafb05
Marcin Warpechowski <warpech@gmail.com>
2017-01-19 10:07:14 +0100

    Switch build.bat to use msbuild 14

--- build.bat
+++ build.bat
@@ -1,23 +1,2 @@
 @echo off
-
-REM Echo Getting Source from GIT
-rem call git --git-dir="%~dp0.git" pull || (GOTO ERROR)
-
-REM call git --git-dir="%~dp0.git" --work-tree="%~dp0" pull && (echo success) || (GOTO ERROR)
-SET FOUND_SOURCE=0
-REM Echo Building Source
-FOR %%i IN (%~dp0src\SignIn\*.csproj) DO ( 
-SET FOUND_SOURCE=1
-REM msbuild %~dp0\src\SignIn\SignIn.csproj && (GOTO ERROR)
-msbuild %%i || (GOTO ERROR)
-)
-
-IF "%FOUND_SOURCE%"=="0" ( echo No source found, check batch argument
-GOTO ERROR  )
-
-GOTO END
-
-:ERROR
-echo Error building!
-
-:END
+"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild" SignIn.sln /t:Rebuild
