214aa677c7b1edfc647f55d546f4680eaf8f56e0
Marcin Warpechowski <warpech@gmail.com>
2017-01-27 11:06:29 +0100

    make the build script work in the directory relative to the bat file
    using (pushd, popd)
    responds to https://github.com/StarcounterPrefabs/SignIn/pull/61#issuecomment-275201520

--- build.bat
+++ build.bat
@@ -1,2 +1,20 @@
-@echo off
-"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild" SignIn.sln
+@ECHO OFF
+
+:: Set up the env to use Msbuild 14.0
+IF "%VS140COMNTOOLS%" EQU "" (
+    SET "ERRORMSG=Msbuild 14.0 seems to be not installed)"
+    SET ERRORLEVEL=2
+    GOTO ERROR
+)
+CALL "%VS140COMNTOOLS%\vsvars32.bat"
+
+PUSHD %~dp0
+msbuild
+POPD
+GOTO END
+
+:ERROR
+ECHO %ERRORMSG%
+EXIT /B %ERRORLEVEL%
+
+:END
