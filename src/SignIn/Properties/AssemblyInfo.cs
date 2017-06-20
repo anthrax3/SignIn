6e0db6044b817b9fb0d64cdf4e24722b1760cb15
Patryk Szwermer <patryk.szwer@gmail.com>
2017-06-20 13:40:55 +0200

    Update version to 5.2.0

--- src/SignIn/Properties/AssemblyInfo.cs
+++ src/SignIn/Properties/AssemblyInfo.cs
@@ -34,8 +34,8 @@ using Starcounter.Internal;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("5.1.1")]
+[assembly: AssemblyVersion("5.2.0")]
+[assembly: AssemblyFileVersion("5.2.0")]
 
 
 // Starcounter Package information
